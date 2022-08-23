ea1794a308568fb2dcce7374240854162fb8f002
lenac0des <ellenajean@gmail.com>
2022-08-23 10:43:20 -0400

    rough

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# raindrops
+This project is inspired by one of my good friends who has trouble sleeping at night.
+
+A rain storm simulator made with mostly Javascript and HTML with a splash of CSS.
+
+
--- main.js
+++ main.js
@@ -1,9 +1,51 @@
-document.getElementById('calm').onclick = calmRain
-document.getElementById('thunder').onclick = thunderRain
-document.getElementById('pit').onclick = pitTer
-document.getElementById('pat').onclick = patTer
 
 
-function calmRain() {
+let message = 'Close your eyes and count the rain drops.'
+
+
+let calmRain = document.getElementById('audioCalm')
+
+let thunderRain = document.getElementById('audioThunder')
+
+let pitTer = document.getElementById('audioPit')
+
+let patTer = document.getElementById('audioPat')
+// document.getElementById('calm').onclick = calmRain
+// document.getElementById('thunder').onclick = thunderRain
+// document.getElementById('pit').onclick = pitTer
+// document.getElementById('pat').onclick = patTer
+
+
+// function calmRain() {
+//     document.querySelector('audio').ul.li = 'calm'
+//     document.querySelector('audio').style.display.toggle('hidden')
+//     document.querySelector('#choice').innerText = message
+
+// }
+
+// function play() {
+//     let audio = document.getElementById('audioCalm')
+//     audio.play()
+//     document.querySelector('#choice').innerText = message
+// }
+
+function currentSound(){
+    if(currentSound === 'audioCalm'){
+    calmRain.play();
+    }else if(currentSound != 'audioCalm'){
+        calmRain.pause();
+    }else if(currentSound === 'audioThunder'){
+        thunderRain.play();
+    }else if(currentSound != 'audioThunder'){
+        thunderRain.pause();
+    }else if(currentSound === 'audioPit'){
+        pitTer.play()
+    }else if(currentSound != 'audioPit'){
+        pitTer.pause();
+    }else if(currentSound === 'audioPat'){
+        patTer.play();
+    }else{
+        patTer.pause();
+    }
     
 }
