560ae4b3a2b52c6adfb9df53129ae300ffb431e5
lenac0des <ellenajean@gmail.com>
2022-08-22 21:32:03 -0400

    added elements to css and js

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('calm').onclick = calmRain
+document.getElementById('thunder').onclick = thunderRain
+document.getElementById('pit').onclick = pitTer
+document.getElementById('pat').onclick = patTer
+
+
+function calmRain() {
+    
+}
