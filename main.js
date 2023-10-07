48e42316fc31e976c817c1bc3d9d9056f8ce6880
lenac0des <ellenajean@gmail.com>
2023-10-07 13:26:32 -0400

    added sounds and images

--- main.js
+++ main.js
@@ -67,3 +67,108 @@ function playRainWindow() {
     }
 }
 windowButton.addEventListener("click", playRainWindow);
+
+let balconyRain = document.getElementById("balcony");
+let balconyButton = document.getElementById("balconyButton");
+
+function playBalconyRain() {
+    if (balconyRain.paused) {
+        balconyRain.play()
+    } else {
+        balconyRain.pause()
+        balconyRain.currentTime = 0;
+    }
+}
+balconyButton.addEventListener("click", playBalconyRain);
+
+let distantThunder = document.getElementById("distant");
+let distantButton = document.getElementById("distantButton");
+
+function playDistantThunder() {
+    if (distantThunder.paused) {
+        distantThunder.play()
+    } else {
+        distantThunder.pause()
+        distantThunder.currentTime = 0;
+    }
+}
+distantButton.addEventListener("click", playDistantThunder);
+
+
+let lightWin = document.getElementById("lightwindow");
+let lightButton = document.getElementById("lightButton");
+
+function playLightWin() {
+    if (lightWin.paused) {
+        lightWin.play()
+    } else {
+        lightWin.pause()
+        lightWin.currentTime = 0;
+    }
+}
+lightButton.addEventListener("click", playLightWin);
+
+let metal = document.getElementById("metal");
+let metalButton = document.getElementById("metalButton");
+
+function playMetal() {
+    if (metal.paused) {
+        metal.play()
+    } else {
+        metal.pause()
+        metal.currentTime = 0;
+    }
+}
+metalButton.addEventListener("click", playMetal);
+
+let softThunder = document.getElementById("softthunder");
+let softButton = document.getElementById("softButton");
+
+function playSoftThunder() {
+    if (softThunder.paused) {
+        softThunder.play()
+    } else {
+        softThunder.pause()
+        softThunder.currentTime = 0;
+    }
+}
+softButton.addEventListener("click", playSoftThunder);
+
+let tent = document.getElementById("tent");
+let tentButton = document.getElementById("tentButton");
+
+function playTent() {
+    if (tent.paused) {
+        tent.play()
+    } else {
+        tent.pause()
+        tent.currentTime = 0;
+    }
+}
+tentButton.addEventListener("click", playTent);
+
+let nature = document.getElementById("nature");
+let natureButton = document.getElementById("natureButton");
+
+function playNature() {
+    if (nature.paused) {
+        nature.play()
+    } else {
+        nature.pause()
+        nature.currentTime = 0;
+    }
+}
+natureButton.addEventListener("click", playNature);
+
+let umbrella = document.getElementById("umbrella");
+let umbrellaButton = document.getElementById("umbrellaButton");
+
+function playUmbrella() {
+    if (umbrella.paused) {
+        umbrella.play()
+    } else {
+        umbrella.pause()
+        umbrella.currentTime = 0;
+    }
+}
+umbrellaButton.addEventListener("click", playUmbrella);
---
+++ songdata.js
@@ -0,0 +1,26 @@
+/**
+    * @description      : 
+    * @author           : ellen
+    * @group            : 
+    * @created          : 07/10/2023 - 11:37:42
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 07/10/2023
+    * - Author          : ellen
+    * - Modification    : 
+**/
+let rainSounds = [
+    {
+        name: 'Light Rain',
+        path: 'sounds/soft.mp3',
+        title: 'Soft Slumber',
+        cover: 'images/leaf.png'
+    },
+    {
+        name: 'Heavy Rain',
+        path: 'sounds/soft.mp3',
+        title: 'Soft Slumber',
+        cover: 'images/leaf.png'
+    }
+]
