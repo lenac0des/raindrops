0bc58af2a11a8c9b303e155fa777cf68e682a4a9
lenac0des <ellenajean@gmail.com>
2023-10-07 14:13:25 -0400

    more data added

--- songdata.js
+++ songdata.js
@@ -18,9 +18,70 @@ let rainSounds = [
         cover: 'images/leaf.png'
     },
     {
-        name: 'Heavy Rain',
-        path: 'sounds/soft.mp3',
-        title: 'Soft Slumber',
-        cover: 'images/leaf.png'
+        name: 'Thunder',
+        path: 'sounds/thunderloud.mp3',
+        title: 'Stormy Rain',
+        cover: 'images/thunder.png'
+    },
+    {
+        name: 'Rainy Roof',
+        path: 'sounds/rainroof.mp3',
+        title: 'Rain on the Roof',
+        cover: 'images/roof.png'
+    },
+    {
+        name: 'Window Rain',
+        path: 'sounds/windowrain.mp3',
+        title: 'Window Hitting the Window',
+        cover: 'images/window.png'
+    },
+    {
+        name: 'Rainy Balcony',
+        path: 'sounds/balcony.mp3',
+        title: 'Cozy Balcony Rain',
+        cover: 'images/balcony.png'
+    },
+    {
+        name: 'Distant Thunder',
+        path: 'sounds/distantthunder.mp3',
+        title: 'Thunder Rolling in the Distance',
+        cover: 'images/stormy.png'
+    },
+    {
+        name: 'Soft Window',
+        path: 'sounds/lightwindow.mp3',
+        title: 'Rain Tapping Lightly on the Window',
+        cover: 'images/sky.png'
+    },
+    {
+        name: 'Rain & Metal Roof',
+        path: 'sounds/metalroof.mp3',
+        title: 'Rain on a Metal Roof',
+        cover: 'images/metal.png'
+    },
+    {
+        name: 'Cozy Thunder',
+        path: 'sounds/softthunder.mp3',
+        title: 'Soft Thunder',
+        cover: 'images/cozy.png'
+    },
+    {
+        name: 'Rain Outdoors',
+        path: 'sounds/tent.mp3',
+        title: 'Rain Hitting a Tent',
+        cover: 'images/tent.png'
+    },
+    {
+        name: 'Birds & Thunder',
+        path: 'sounds/thundernature.mp3',
+        title: 'Rain Forest Thunder',
+        cover: 'images/bird.png'
+    },
+    {
+        name: 'Umbrellas & Rain',
+        path: 'sounds/umbrella.mp3',
+        title: 'Umbrellas in the Rain',
+        cover: 'images/umbrella.png'
     }
+
 ]
